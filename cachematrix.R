951440529b10a7a6fd9c57a250c22e4fae1304f8
lsfrf <45527803+lsfrf@users.noreply.github.com>
2018-12-02 13:46:08 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## use a given matrix to produce a special object
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        reverse <- NULL
+        set <- function(y) {
+                x <<- y
+                reverse <<- NULL
+        }
+        get <- function() x
+        setreverse <- function(reverse) reverse <<- reverse
+        getreverse <- function() reverse
+        list(set = set, get = get,
+             setreverse = setreverse,
+             getreverse = getreverse)
 
 }
 
 
-## Write a short comment describing this function
+## calculate the reverse, if already calculated, return cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     
+         reverse <- x$getreverse()
+        if(!is.null(reverse)) {
+                message("getting cached data")
+                return(reverse)
+        }
+        data <- x$get()
+        reverse <- solve(data, ...)
+        x$setreverse(reverse)
+       
 }
